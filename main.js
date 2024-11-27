e071f220fda801540063fc98db6c4814a13917b1
Avaz <avazbekrozimboyev08@gmail.com>
2024-11-27 12:49:42 +0500

    Java

---
+++ main.js
@@ -0,0 +1,56 @@
+//1-masala
+// let son = prompt("son");
+// if(son >= 50 && son <= 100){
+//     console.log("50 va 100 orasida");
+// }else{
+//     console.log("50 va 100 oraliqda emas");
+// }
+
+// 2-masala
+// let son1 = prompt("son1");
+// console.log("engkichcigi" + Math.floor(son1));
+// console.log("engkattasi" + Math.ceil(son1));
+
+//3-masala
+// son = Math.random() * 100;
+// jyt = Math.round(son);
+// if (jyt % 2 == 0) {
+//   console.log(jyt + `juf son`);
+// } else {
+//   console.log(jyt + `toq son`);
+// }
+
+//4-masala
+// let radius = prompt("son");
+// let diameter = 2 * radius;
+// let L = 2 * Math.PI * radius;
+// console.log(`Diametr: ${diameter}`);
+// console.log(`Doira uzunligi: ${L}`);
+
+//5-masala
+// let a1 = prompt("son1");
+// let a2 = prompt("son2");
+// let a3 = prompt("son3");
+// let a4 = prompt("son4");
+// console.log(Math.max(a1, a2, a3, a4));
+
+//6-masala
+// let son1 = prompt("son1");
+// let son2 = prompt("son2");
+// let son = son1 * son2;
+// let son3 = son1 / son2;
+// if (son1 > son2) {
+//   console.log("Ko'paytma: " + son);
+// } else  {
+//   console.log("Bo'lnma: " + son3);
+// }
+
+// 7-masala
+
+// let number = prompt("number");
+// let umumiy = Math.sqrt(number);
+// if (umumiy % 1 == 0) {
+//   console.log("butun son ");
+// } else {
+//   console.log("butun son emas ");
+// }
